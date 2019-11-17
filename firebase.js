a4a64cb8645f4a72abba6fddd3401da37c9a1307
Admin <admin@admin.com>
2019-11-17 12:26:33 +0100

    added firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: "AIzaSyAiro_Ma1VKAFX93hGCy2CjmCTY9FtArxM",
+  authDomain: "build-test-app.firebaseapp.com",
+  databaseURL: "https://build-test-app.firebaseio.com",
+  projectId: "build-test-app",
+  storageBucket: "build-test-app.appspot.com"
+};
+
+export default config;
